27b88b8be6f5bfdf212107fa1a73211e0963df19
abhaytupe <tupeabhay718@gmail.com>
2026-05-10 22:32:05 +0530

    Update PR_9.py

--- PR_9.py
+++ PR_9.py
@@ -1,3 +1,4 @@
+#Data Visualization II :  Use the inbuilt dataset 'titanic' as used in the above problem. Plot a box plot for distribution of age with respect to each gender along with the information about whether they survived or not. (Column names : 'gender' and 'age').Write observations on the inference from the above statistics. 
 # Step_1
 import seaborn as sns
 import matplotlib.pyplot as plt
